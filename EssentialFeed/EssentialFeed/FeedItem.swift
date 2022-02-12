da6850941cbfbfaa946f203213b630ec3c593529
Daren Hayward <darhaywa@cisco.com>
2022-02-12 08:35:56 +0000

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Daren Hayward on 12/02/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
