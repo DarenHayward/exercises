4441a12a8ae43c4336e5d6d23214e05cc7af823b
Daren Hayward <darhaywa@cisco.com>
2022-02-13 08:08:27 +0000

    Request data on 'load' using a Singleton approach

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,18 +8,35 @@
 import XCTest
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_init_requestDataFromURL() {
+        // Arange
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        // Act
+        sut.load()
+        
+        // Assert
+        XCTAssertNotNil(client.requestedURL)
     }
 }
