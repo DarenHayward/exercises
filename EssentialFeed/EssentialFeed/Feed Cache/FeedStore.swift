f373a7fa99006d42bb83d4ae68168f3b551f9570
Daren Hayward <daren@hayward.uk.com>
2022-02-24 08:07:21 +0000

    Fixed typo

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum RetreivedCachedFeedResult {
+public enum RetrievedCachedFeedResult {
     case empty
     case found(feed: [LocalFeedImage], timestamp: Date)
     case failure(Error)
@@ -16,7 +16,7 @@ public enum RetreivedCachedFeedResult {
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetreivedCachedFeedResult) -> Void
+    typealias RetrievalCompletion = (RetrievedCachedFeedResult) -> Void
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
