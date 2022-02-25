7778998fc20d11ce1341fda41f12463504ea5aa1
Daren Hayward <daren@hayward.uk.com>
2022-02-25 14:53:18 +0000

    Replace production store URL with a test-specific store URL to avoid sharing state/artifacts with other parts of the system (including other tests!)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -66,14 +66,14 @@ class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        let storeURL = storeURL()
+        let storeURL = testSpecificStoreURL()
         try? FileManager.default.removeItem(at: storeURL)
     }
 
     override func tearDown() {
         super.tearDown()
 
-        let storeURL = storeURL()
+        let storeURL = testSpecificStoreURL()
         try? FileManager.default.removeItem(at: storeURL)
     }
 
@@ -145,14 +145,14 @@ class CodableFeedStoreTests: XCTestCase {
 
     // MARK: - Helpers
     private func makeSUT() -> CodableFeedStore {
-        let storeURL = storeURL()
+        let storeURL = testSpecificStoreURL()
 
         let sut = CodableFeedStore(storeURL: storeURL)
         trackForMemoryLeaks(sut)
         return sut
     }
 
-    private func storeURL() -> URL {
-        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    private func testSpecificStoreURL() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
     }
 }
