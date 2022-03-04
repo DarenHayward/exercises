548006673cc58130ab0ed6afd4d05e2f40d7c416
Daren Hayward <daren@hayward.uk.com>
2022-03-04 07:03:38 +0000

    Replace occurrences of `Error?` for representing operation success/failure with `Result<Void, Error>`

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -10,10 +10,10 @@ import Foundation
 public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
-    typealias DeletionError = Error?
-    typealias DeletionCompletion = (DeletionError) -> Void
+    typealias DeletionResult = Result<Void, Error>
+    typealias DeletionCompletion = (DeletionResult) -> Void
 
-    typealias InsertionResult = Error?
+    typealias InsertionResult = Result<Void, Error>
     typealias InsertionCompletion = (InsertionResult) -> Void
 
     typealias RetrievalResult = Result<CachedFeed?, Error>
