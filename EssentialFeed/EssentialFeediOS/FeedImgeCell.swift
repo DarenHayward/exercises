2f7b82cc74d435e274cc098a1e2d12039bb2c750
Daren Hayward <daren@hayward.uk.com>
2022-03-08 05:56:13 +0000

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImgeCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImgeCell.swift
+//  EssentialFeediOS
+//
+//  Created by Daren Hayward on 08/03/2022.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
