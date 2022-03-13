0e3515146037bd90ba83978c91046bd58bce0ca9
Daren Hayward <daren@hayward.uk.com>
2022-03-13 11:04:07 +0000

    Add UITableView helper to dequeue typed cells by class name

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/UIView+Dequeueing.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Dequeueing.swift
+//  EssentialFeediOS
+//
+//  Created by Daren Hayward on 13/03/2022.
+//
+
+import UIKit
+
+extension UITableView {
+    func dequeueReusableCell<T: UITableViewCell>() -> T {
+        let identifier = String(describing: T.self)
+        return dequeueReusableCell(withIdentifier: identifier) as! T
+    }
+}
