20726864b4a518dea5e48c78770cc37928ef7315
Daren Hayward <daren@hayward.uk.com>
2022-03-19 16:16:14 +0000

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Daren Hayward on 19/03/2022.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
