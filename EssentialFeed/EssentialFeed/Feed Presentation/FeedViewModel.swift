9fc34a375e420eb549ad5c330d575a7f3ebf38a2
Daren Hayward <daren@hayward.uk.com>
2022-03-19 16:15:22 +0000

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Daren Hayward on 19/03/2022.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
