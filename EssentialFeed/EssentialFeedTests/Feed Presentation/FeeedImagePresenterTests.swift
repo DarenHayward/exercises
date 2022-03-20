a1a9a5c0d0ed96e20dd18938785e4b17befa97ce
Daren Hayward <daren@hayward.uk.com>
2022-03-20 07:02:46 +0000

    Extract `makeSUT` method into test helper.

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeeedImagePresenterTests.swift
@@ -16,13 +16,20 @@ final class FeedImagePresenter {
 class FeedImagePresenterTests: XCTestCase {
 
     func test_init_doesNotSendImagesToView() {
-        let view = ViewSpy()
-        _ = FeedImagePresenter(view: view)
+        let (_, view) = makeSUT()
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view images")
     }
 
     // MARK: - Helpers
+
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view)
+        trackForMemoryLeaks(sut)
+        return (sut, view)
+    }
 
     class ViewSpy {
         let messages = [Any]()
