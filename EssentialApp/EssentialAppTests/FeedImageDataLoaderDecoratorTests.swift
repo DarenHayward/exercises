5b5bf40bc78b1c9565e2a7d618dc897ff25040c4
Daren Hayward <daren@hayward.uk.com>
2022-04-05 08:09:35 +0100

    Extract `FeedImageDataLoader` test helpers into a shared protocol extension to remove duplication

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderDecoratorTests.swift
@@ -20,7 +20,7 @@ class FeedImageDataLoaderDecorator: FeedImageDataLoader {
     }
 }
 
-class FeedImageDataLoaderDecoratorTests: XCTestCase {
+class FeedImageDataLoaderDecoratorTests: XCTestCase, FeedImageDataLoaderTestCase {
 
     func test_init_doesNotLoadImageData() {
         let (_, loader) = makeSUT()
@@ -72,29 +72,6 @@ class FeedImageDataLoaderDecoratorTests: XCTestCase {
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
-    }
-
-    private func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-
-        _ = sut.loadImageData(from: anyURL()) { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedFeed), .success(expectedFeed)):
-                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-
-            case (.failure, .failure):
-                break
-
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-
-            exp.fulfill()
-        }
-
-        action()
-
-        wait(for: [exp], timeout: 1.0)
     }
 
     private func anyData() -> Data {
