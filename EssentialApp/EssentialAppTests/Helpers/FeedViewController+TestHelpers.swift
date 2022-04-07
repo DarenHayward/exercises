c1cb55203c01edb8e9be6fbc2a0ec3234127c323
Daren Hayward <daren@hayward.uk.com>
2022-04-07 08:35:44 +0100

    Displays cached feed on launch when customer has no connectivity

--- EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -43,6 +43,10 @@ extension FeedViewController {
         dataStore?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
 
+    func renderedFeedImageData(at index: Int) -> Data? {
+        return simulateFeedImageViewVisible(at: index)?.renderedImage
+    }
+
     var errorMessage: String? {
         return errorView?.message
     }
